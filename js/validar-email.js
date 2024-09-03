9b5eb52e65bcd2038df3f594158f5336e5224054
danielveg4 <dvgilles@gmail.com>
2024-09-03 18:09:49 +0200

    g

--- js/validar-email.js
+++ js/validar-email.js
@@ -1,8 +1,8 @@
-import { validateEmail } from "./modules/validate-email.js";
+import { validateEmail } from "./modules/validate-email.js"; 
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('email').addEventListener('input', (e) => {
         let msg = validateEmail(e.target.value) ? 'Email válido' : 'Email inválido';
-        document.getElementById('email-msg').textContent = msg;
+        document.getElementById('msg').textContent = msg; 
     });
 });
