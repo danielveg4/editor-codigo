7cf335d995f5936d70ed25abba91c865eefad7d7
danielveg4 <dvgilles@gmail.com>
2024-09-06 12:44:50 +0200

    del

--- js/modules/validate-email.js
+++
@@ -1,6 +0,0 @@
-const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-
-export const validateEmail = (email) => {
-    if(!email || email == '') return false;
-    return emailReg.test(email);
-}
--- js/validar-email.js
+++
@@ -1,9 +0,0 @@
-import { validateEmail } from "./modules/validate-email.js"; 
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('email').addEventListener('input', (e) => {
-        let msg = validateEmail(e.target.value) ? 'Email válido' : 'Email inválido';
-        document.getElementById('msg').textContent = msg; 
-    });
-});
-
