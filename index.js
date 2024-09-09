13549202e167b8dede3c605b0efc34fe8974f4e0
danielveg4 <dvgilles@gmail.com>
2024-09-09 10:29:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 /* 
-# yarn
-yarn install
-yarn start
-# npm
-npm install
-npm start
 
----cypress---
-
-npm install cypress --save-dev
-npx cypress run
---> script start en el package
-
-ejemplo en integration: 00-basic.spec.cy.js
-
-hay un ejemplo más completo en 00-bdd.spec.cy.js
-
-https://github.com/EscuelaIt/cypress-e2e
 
 */
 
