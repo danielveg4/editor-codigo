13c5053dff6e919c049bec82baf2c469d988d36e
danielveg4 <dvgilles@gmail.com>
2024-09-16 17:51:56 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-/*  */
+const textElement = document.getElementById('text-code');
+const viewElement = document.getElementById('view')
+
+const update = () => {
+    console.log(textElement.value);
+    viewElement.srcdoc = textElement.value;
+}
+
+textElement.addEventListener('keydown', update);
